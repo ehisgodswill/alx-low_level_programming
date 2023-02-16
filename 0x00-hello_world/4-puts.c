5c21911da18a37908c6e534884b2e834d156330a
ehisgodswill <ehisgodswill@gmail.com>
2023-02-16 17:52:20 +0100

    fix - betty 4-5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * function - prints a string output
+ * 
+ * return - exit point
+*/
 int main(void) {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * function - prints a string output
+ * 
+ * return - exit point
+*/
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
