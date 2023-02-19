7b5230dca885c6f225ff483c4cd5a71eec909a6a
ehisgodswill <ehisgodswill@gmail.com>
2023-02-19 22:51:46 +0100

    update 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,34 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
- * 
+ *
  * Return: exit point
 */
 
 int main(void)
 {
-	
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int x = n % 10;
+	switch(x)
+	{
+		case x > 5 :
+		print("Last digit of %d is %d and is greater than 5\n", n,x);
+		break;
+		case x == 0 :
+		print("Last digit of %d is %d and is greater than 5\n", n,x);
+		break;
+		case x < 6 && x != 0 :
+		print("Last digit of %d is %d and is greater than 5\n", n,x);
+		break;
+		default:
+		break; 
+
+	}
+	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,19 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
- * 
+ *
  * Return: exit point
 */
 
 int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	
+	return (0);
 }
