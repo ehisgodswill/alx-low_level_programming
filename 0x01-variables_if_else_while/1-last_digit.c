6dda0ad74a1e87c91f2917a38bb4b871a25b6c4b
ehisgodswill <ehisgodswill@gmail.com>
2023-02-19 23:01:26 +0100

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,20 +16,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
-	switch(x)
-	{
-		case x > 5 :
-		print("Last digit of %d is %d and is greater than 5\n", n,x);
-		break;
-		case x == 0 :
-		print("Last digit of %d is %d and is greater than 5\n", n,x);
-		break;
-		case x < 6 && x != 0 :
-		print("Last digit of %d is %d and is greater than 5\n", n,x);
-		break;
-		default:
-		break; 
 
-	}
+	if x > 5 :
+		print("Last digit of %d is %d and is greater than 5\n", n,x);
+	else if x == 0 :
+		print("Last digit of %d is %d and is greater than 5\n", n,x);
+	else if x < 6 && x != 0 :
+		print("Last digit of %d is %d and is greater than 5\n", n,x);
+
 	return (0);
 }
