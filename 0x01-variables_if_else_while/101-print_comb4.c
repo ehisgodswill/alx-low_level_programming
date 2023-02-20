03eba21e31b602c5632cb8016586bf0787cdb25d
ehisgodswill <ehisgodswill@gmail.com>
2023-02-20 16:04:59 +0100

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,7 +24,7 @@ int main(void)
 						putchar(a);
 						putchar(b);
 						putchar(c);
-						if (a == 7; b == 8; c == 9 )
+						if (a == 7 && b == 8 && c == 9)
 							continue;
 						else
 						{
