bfd6c02edbef2957778f9fff8aba847a9034781c
ehisgodswill <ehisgodswill@gmail.com>
2023-02-21 18:41:50 +0100

    3,6,7

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,6 +10,7 @@
 int _islower(int c)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == c)
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,6 +12,7 @@ int print_last_digit(int r)
 
 	if (n < 0)
 		n *= -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	n %= 10;
+	_putchar(n + '0');
+	return (n);
 }
