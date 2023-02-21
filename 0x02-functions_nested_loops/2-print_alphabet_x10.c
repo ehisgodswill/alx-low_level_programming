a4e0b6f1f6b914233ef9327be08bb22ebbff4e9a
ehisgodswill <ehisgodswill@gmail.com>
2023-02-21 17:34:42 +0100

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char j;
 	
 	for (i = 0; i < 10; i++)
-		print_alphabet();
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(i);
+		_putchar('\n');
+	}
 }
