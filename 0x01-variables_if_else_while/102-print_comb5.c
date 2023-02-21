14a0af0f333c8a5e93444f8d3582148237ccfb1b
ehisgodswill <99468350+ehisgodswill@users.noreply.github.com>
2023-02-21 12:53:09 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,12 +8,27 @@
  * Return: exit point
 */
 
-int main(void)
+int main()
 {
-	int n;
+	int a, b;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a + 1;b <= 99; b++)
+		{
+			if (a == b)
+				continue;
+
+			putchar((a/10) + '0');
+			putchar((a%10) + '0');
+			putchar(' ');
+			putchar((b/10) + '0');
+			putchar((b%10) + '0');
+
+			if (a != 98 || b != 99)
+				printf(", ");
+		}
+	}
+	putchar('\n');
 	return (0);
 }
