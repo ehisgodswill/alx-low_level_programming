f91b4f18bdcb01fce6366abe9568a4b0b5285343
ehisgodswill <99468350+ehisgodswill@users.noreply.github.com>
2023-02-21 09:55:53 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,25 +13,21 @@ int main(void)
 	int a, b, c;
 	for (a = '0'; a <= '9'; a++)
 	{
-		if (a != b)
+		for (b = a + 1; b <= '9'; b++)
 		{
-			for (b = a + 1; b <= '9'; b++)
+			for (c = b + 1; c <= '9'; c++)
 			{
-				if (b != c)
+				if ((b != c) != a)
 				{
-					for (c = b + 1; c <= '9'; c++)
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == '7' && b == '8' && c == '9')
+						continue;
+					else
 					{
-						putchar(a);
-						putchar(b);
-						putchar(c);
-						if (a == '7' && b == '8' && c == '9')
-							continue;
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
