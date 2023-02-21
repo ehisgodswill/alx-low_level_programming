3bfe80b20e389bfc5246a40db6865db7454ddd75
ehisgodswill <99468350+ehisgodswill@users.noreply.github.com>
2023-02-21 15:24:07 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,11 @@
 
 int main(void)
 {
-	char j[8] = '_putchar';
+	char j[8] = "_putcharv";
 	int i;
 
 	for (i = 0; i < 8; i++)
-		_putchar(word[i]);
+		_putchar(j[i]);
 	_putchar('\n');
 	return 0;
 }
