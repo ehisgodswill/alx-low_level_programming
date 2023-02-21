30b3f1bbe250b8f4050b74049bff624cc3a85d4b
ehisgodswill <ehisgodswill@gmail.com>
2023-02-21 17:19:26 +0100

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ * Description: print using the print function
+ * Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
