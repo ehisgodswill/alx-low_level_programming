b4d0c7f6d1a99f135192fd050593e498ef068d9a
ehisgodswill <ehisgodswill@gmail.com>
2023-02-22 01:22:54 +0100

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j = 1, k = 2, r;
+	/*
+	long int fibonacci[50];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	*/
+	printf("%d, %d, ", j, k);
+	for (i = 0; i < 98; i++)
+	{
+		r = j + k;
+		j = k;
+		k = r;
+		if (i == 97)
+			printf("%d\n", r);
+		else
+			printf("%d, ", r);
+	}
+
+	return (0);
+}
