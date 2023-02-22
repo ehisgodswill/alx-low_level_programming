c08f3673ad5d4e55e1cc2f5b28c5a49ae4ee0278
ehisgodswill <ehisgodswill@gmail.com>
2023-02-22 21:58:15 +0100

    m

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i;
-	unsigned long int p = 1, q = 2, r;
+	float int p = 1, q = 2, r;
 
 	printf("%lu, %lu, ", p, q);
 	for (i = 2; i < 98; i++)
@@ -18,9 +18,9 @@ int main(void)
 		p = q;
 		q = r;
 		if (i == 97)
-			printf("%lu\n", r);
+			printf("%f\n", r);
 		else
-			printf("%lu, ", r);
+			printf("%f, ", r);
 	}
 
 	return (0);
