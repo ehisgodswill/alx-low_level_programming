2199c5a58b8b579ac456a6d1a3572fd1781e4ec5
ehisgodswill <ehisgodswill@gmail.com>
2023-02-22 23:34:39 +0100

    104 new code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,19 +9,32 @@
 int main(void)
 {
 	int i;
-	double p = 1, q = 2, r;
+	unsigned long int xf = 0, xb = 1, yf = 0, yb = 2;
+	unsigned long int rf = 0, rb = 0, div = 10000000000;
 
-	printf("%.0f, %.0f, ", p, q);
+	printf("%lu, %lu, ", xb, yb);
 	for (i = 2; i < 98; i++)
 	{
-		r = p + q;
-		p = q;
-		q = r;
-		if (i == 97)
-			printf("%.0f\n", r);
-		else
-			printf("%.0f, ", r);
-	}
+		if (xb + yb >= div || rf > 0)
+		{
+			rf = xf + yf + ((xb + yb) / div);
+			rb = (xb + yb) % div;
+			printf("%lu%010lu", rf, rb);
+			xf = yf, xb = yb;
+			yf = rf, yb = rb;
 
+		}
+		else
+		{
+			rb = xb + yb;
+			xb = yb, yb = rb;
+			printf("%lu", rb);
+
+		}
+
+		if (i != 97)
+			printf(", ");
+	}
+	printf("\n");
 	return (0);
 }
