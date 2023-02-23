167dcbc2bb5b66d77738453afc36f52b9ea0c749
ehisgodswill <99468350+ehisgodswill@users.noreply.github.com>
2023-02-23 12:34:22 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,17 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i < 100 ; i++)
+	for (; i <= 100 ; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if ((i % 5 != 0) && (i % 3 != 0))
 			printf("%d ", i);
+		printf(' ');
 	}
-	printf("Buzz\n");
+	printf("\n");
 
 	return (0);
 }
