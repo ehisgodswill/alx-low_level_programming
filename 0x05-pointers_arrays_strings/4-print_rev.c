dd47de3b3d5ed246a28d7891b1fd9ebe03859060
ehisgodswill <ehisgodswill@gmail.com>
2023-02-27 16:28:41 +0100

    4 try

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,19 @@
 #include "main.h"
-/**
- * m -
- * @:
- * Return:
-*/
-{
 
+/**
+ * print_rev - prints a string in reverse
+ * @s: string variable
+ * Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
 }
