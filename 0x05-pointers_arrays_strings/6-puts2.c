df5542e401c8f46f1ee66b4fbe512f947e88b713
ehisgodswill <ehisgodswill@gmail.com>
2023-02-27 19:05:26 +0100

    try b6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,17 @@
 #include "main.h"
 /**
- * m -
- * @:
- * Return:
+ * puts2 - a function that prints a string, followed by a new line, to stdout
+ * @str: string to output
+ * Return: nothing
 */
+void puts2(char *str)
 {
+	int i = 0;
 
+	while (str[i++] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
