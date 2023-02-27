58dd4caf6e48b41e98942bbc41f8cadb8169c8d7
ehisgodswill <ehisgodswill@gmail.com>
2023-02-27 20:22:21 +0100

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,24 @@
 #include "main.h"
 /**
- * m -
- * @:
- * Return:
+ * puts_half - puts half of the string
+ * @str: string
+ * Return: nothing
 */
+void puts_half(char *str)
 {
+	int len = 0, i, n;
 
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
