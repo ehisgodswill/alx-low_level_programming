f797ebfdbb2146cf3e1adde8295a023ecc0df7b5
ehisgodswill <ehisgodswill@gmail.com>
2023-02-27 16:46:20 +0100

    5 try

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,22 @@
 #include "main.h"
-/**
- * m -
- * @:
- * Return:
-*/
-{
 
+/**
+ * rev_string - function that reverse string
+ * @s: string
+ * Return: nothing
+*/
+void rev_string(char *s)
+{
+	char t = *s;
+	int i, j = 0;
+
+	while (s[i] != '\0')
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		t[j] = s[i];
+		j++;
+		i--;
+	}
 }
