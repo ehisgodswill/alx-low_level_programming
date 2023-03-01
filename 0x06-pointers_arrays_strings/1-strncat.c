5c3b6518e85dc4c6bf0dc3dfead8c9b3233e0317
ehisgodswill <ehisgodswill@gmail.com>
2023-03-01 23:18:23 +0100

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - similar to strcat function
+ * @dest: char destination
+ * @src: source
+ * @N: number
+ * Return: character
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i++] = src[j++];
+		if (j == n)
+			break;
+	}
+	return (dest);
+}
