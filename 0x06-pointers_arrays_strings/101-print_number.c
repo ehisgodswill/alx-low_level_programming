ad3660304f13d757711a5380e84bab4fd4856aba
ehisgodswill <ehisgodswill@gmail.com>
2023-03-02 16:24:51 +0100

    mm

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,35 @@
 #include "main.h"
 
 /**
- * f -
- * @:
- * Return:
-*/
-
+ * print_number - prints integers enters as parameters using putchar
+ * @n: integer to print
+ * Return: void
+ */
+void print_number(int n)
 {
+	int power;
 
+	power = 10000000;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n == 0)
+		_putchar('0');
+
+	else
+	{
+		while (n / power == 0)
+			power /= 10;
+
+		while (power >= 1)
+		{
+			_putchar((n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
+	}
 }
