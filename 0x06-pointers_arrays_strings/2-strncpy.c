9b2e197863a69762dbc2547df86d68a02fd44e92
ehisgodswill <ehisgodswill@gmail.com>
2023-03-02 07:47:45 +0100

    chg

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	char *temp = dest;
 
-	while(*src && i < n)
+	while (*src && i < n)
 	{
 		*dest++ = *src++;
 		i++;
 	}
-	while(i < n)
+	while (i < n)
 	{
 		*dest++ = '\0';
 		i++;
