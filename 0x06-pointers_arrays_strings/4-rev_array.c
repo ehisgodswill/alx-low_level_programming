cb793a1cc65240f0010882cb36b17c8fde898a66
ehisgodswill <ehisgodswill@gmail.com>
2023-03-02 08:26:09 +0100

    chg

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 /**
- * f -
- * @:
- * Return:
+ * reverse_array - function 
+ * @a: integer a
+ * @n: integer n
+ * Return: nothing
 */
-
+void reverse_array(int *a, int n)
 {
-
+	int t, i = 0;
+	while (i < n-- / 2)
+	{
+		t = a[i];
+		a[i++] = a[n];
+		a[n] = t;
+	}
 }
