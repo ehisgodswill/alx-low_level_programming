d26cdbb1be03530d3b13affc3da8a7c0e819b617
ehisgodswill <ehisgodswill@gmail.com>
2023-03-02 07:45:06 +0100

    chg

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
 /**
- * f -
- * @:
- * Return:
+ * *_strncpy - function name
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: char
 */
-
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
+	char *temp = dest;
 
+	while(*src && i < n)
+	{
+		*dest++ = *src++;
+		i++;
+	}
+	while(i < n)
+	{
+		*dest++ = '\0';
+		i++;
+	}
+	return (temp);
 }
