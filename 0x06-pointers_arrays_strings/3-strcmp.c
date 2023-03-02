0089602e00137fddebad542ed0093881abc802c9
ehisgodswill <ehisgodswill@gmail.com>
2023-03-02 08:09:11 +0100

    chg

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * f -
- * @:
- * Return:
+ * _strcmp - compares string
+ * @s1: string
+ * @s2: string
+ * Return: integer
 */
-
+int _strcmp(char *s1, char *s2)
 {
+int r = 0, i = 0;
 
+	while (r == 0)
+	{
+		if (s1[i] == '\0')
+			break;
+		r = s1[i] - s2[i];
+		i++;
+	}
+	return(r);
 }
