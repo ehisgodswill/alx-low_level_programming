890e1b71a854101d2577a674c4b8bdabead7b062
ehisgodswill <ehisgodswill@gmail.com>
2023-03-02 09:15:17 +0100

    updt

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * f -
- * @:
- * Return:
+ * *string_toupper - function
+ * @str: string
+ * Return: string
 */
-
+char *string_toupper(char *str)
 {
-
+	char *temp = str;
+	while (*str)
+	{
+		for (int c = 'a'; c <= 'z'; c++)
+		{
+			if (str == c)
+				str -= 32;
+		}
+		str++;
+	}
+	return (temp);
 }
