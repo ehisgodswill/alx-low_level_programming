e73833ff8df28cf3d19732667e65d48d3f33913b
ehisgodswill <ehisgodswill@gmail.com>
2023-03-06 15:44:20 +0100

    0

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
- * f -
- * @:
- * Return:
+ * *infinite_add - add infinite numbers
+ * @n1: integer
+ * @n2: integer
+ * @r: result
+ * @size_r: size of buffer
+ * Return: result
 */
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int len1 = 0, len2 = 0, len;
 
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+	if (len1 > len2)
+		len = len1;
+	else
+		len = len2;
+	if (len >= size_r)
+		return (0);
 }
