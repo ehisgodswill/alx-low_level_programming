0af293d1920c8e17e472e5fcd685ab399816340f
ehisgodswill <ehisgodswill@gmail.com>
2023-03-08 19:01:03 +0100

    0 - 4

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,16 @@
 #include "main.h"
-
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: An input string to printing
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
 {
-
+	if (*s)
+	{
+		_putchar(*s);
+		puts_recursion(*s + 1);
+	}
+	else
+		_putchar('\n');
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * factorial - a function that returns the factorial of a number.
+ * @n: An input integer
+ * Return: The factorial of n
+ */
+int factorial(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
