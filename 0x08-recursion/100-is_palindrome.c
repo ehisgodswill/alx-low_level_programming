02da633e0a918e015fcfda69462c32f2bc730645
ehisgodswill <ehisgodswill@gmail.com>
2023-03-08 23:29:45 +0100

    STILL A FRAUD

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -24,8 +24,8 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-	if(*s)
-		return (1 + _strlen_recursion(s+1));
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
 
 	return (0);
 }
