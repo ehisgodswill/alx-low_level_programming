5fed321349da8fbb24340c66e1cba8f3bd79ec5c
ehisgodswill <ehisgodswill@gmail.com>
2023-03-10 16:23:32 +0100

    try

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - similar to strcat function
+ * @dest: char destination
+ * @src: source
+ * @n: number
+ * Return: character
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks if char is lower
+ * @c: an input char
+ * Description: a function for lower case character
+ * Return: an integer, 1 if true, 0 if false
+*/
+
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			return (1);
+	}
+	return (0);
+}
