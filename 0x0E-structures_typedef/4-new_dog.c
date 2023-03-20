823fdca8ab570cfdb3f4d90b84a154c1958f207d
ehisgodswill <ehisgodswill@gmail.com>
2023-03-20 17:46:03 +0100

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,38 +11,17 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	dog_t *newdog;
+	char *namecpy, *ownercpy;
 
-	if (name == NULL || owner == NULL)
+	newdog = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL || newdog == NULL)
 		return (NULL);
+	namecpy = strdup(name);
+	ownercpy = strdup(owner);
 
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
-		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
-
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
-		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
-
-	new_dog->name = cpyname;
-	new_dog->age = age;
-	new_dog->owner = cpyowner;
-	return (new_dog);
+	newdog->name = namecpy;
+	newdog->age = age;
+	newdog->owner = ownercpy;
+	return (newdog);
 }
