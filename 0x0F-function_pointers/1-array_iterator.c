2993678b1dbcd6b22597ff48fbb3bbde6786eb08
ehisgodswill <ehisgodswill@gmail.com>
2023-03-22 16:05:48 +0100

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (action == NULL || array == NULL)
 		return;
 
-	for (; i < size; i++)
-		action(array[i]);
+	while (i < size)
+		action(array[i++]);
 }
