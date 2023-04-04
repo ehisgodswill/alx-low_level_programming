5a76293764536c9c755de31f2dcef5f633ffc434
ehisgodswill <ehisgodswill@gmail.com>
2023-04-04 18:53:34 +0100

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,12 +15,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 	newnode = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (newnode == NULL)
 		return (NULL);
 	while (i++ < idx && *head)
 		*head = (*head)->next;
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
 }
