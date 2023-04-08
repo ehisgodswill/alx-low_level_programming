2e102cf09c14ca3ed4d776b66b74e835826f6ff3
ehisgodswill <ehisgodswill@gmail.com>
2023-04-08 22:28:56 +0100

    hhh

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void print_binary(unsigned long int n)
 	unsigned int max = sizeof(unsigned long int) * 8, flag = 0;
 
 	if (n == 0)
-		putchar('0');
+		_putchar('0');
 	while (max--)
 	{
 		int b = (n >> max) & 1;
@@ -18,28 +18,8 @@ void print_binary(unsigned long int n)
 		if (b && !flag)
 			flag = 1;
 		if (!b && flag)
-			putchar('0');
-		else if (b && flag)
-			putchar('1');
-	}
-}
-{
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
 			_putchar('0');
-		else if ((n & max) != 0)
-		{
+		else if (b && flag)
 			_putchar('1');
-			flag = 1;
-		}
-		max >>= 1;
 	}
 }
