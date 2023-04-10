322fc0ad47a7126473903288d4e5afa97a87fe4d
ehisgodswill <ehisgodswill@gmail.com>
2023-04-10 23:37:02 +0100

    m

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <sys/uio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
 ssize_t read_textfile(const char *filename, size_t letters);
