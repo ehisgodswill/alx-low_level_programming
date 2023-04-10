c0347dda0dac9563cfa158b1816351305830da5b
ehisgodswill <ehisgodswill@gmail.com>
2023-04-10 08:32:58 +0100

    100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - A function that checks endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 0x0102;
+	char *c = (char *)&i;
+
+	if (*c == 0x02)
+		return (1);
+	return (0);
+}
