efba36c8f18d4761fc7cdad5c09aab3b6472b222
ehisgodswill <ehisgodswill@gmail.com>
2023-04-11 00:33:41 +0100

    jk

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,4 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 int safe_close(int);
 /**
