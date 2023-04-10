fc21298ceb7c551c33e8d3f5d9b6bd8e2410d409
ehisgodswill <ehisgodswill@gmail.com>
2023-04-10 07:27:55 +0100

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (bit == 0x00 || index > sizeof(unsigned long int) * 8)
 		return (-1);
 	*n &= ~bit;
-	if (*n & bit == 0)
+	if ((*n & bit) == 0)
 		return (1);
 	return (-1);
 }
