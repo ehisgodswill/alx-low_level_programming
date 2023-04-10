a48441efd81729b5ff2041fbfcba6014795beaa7
ehisgodswill <ehisgodswill@gmail.com>
2023-04-10 20:06:00 +0100

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,19 +19,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL || temp == NULL)
 		return (0);
-	fdo = open(filename, O_READONLY);
+	fdo = open(filename, O_RDONLY);
 	if (fdo < 0)
 	{
 		free(temp);
 		return (0);
 	}
-	fdr = read(fdo, temp, letters)
+	fdr = read(fdo, temp, letters);
 	if (fdr < 0)
 	{
 		free(temp);
 		return (0);
 	}
-	fdw = write(STDOUT_FILENO, temp, fdr)
+	fdw = write(STDOUT_FILENO, temp, fdr);
 	free(temp);
 	close(fdo);
 	if (fdw < 0)
