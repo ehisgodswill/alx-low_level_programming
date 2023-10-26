f7ea343789ffd16778f13746afe730910627345c
ehisgodswill <ehisgodswill@gmail.com>
2023-10-26 18:55:53 +0100

    2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,6 +4,33 @@
  * print_binary - A function that prints binary from integer
  * @n: integer
  * Return: none
-*/
-void print_binary(unsigned long int n){
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int t = n;
+	int s = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (t)
+	{
+		t >>= 1;
+		s++;
+	}
+
+	while (--s >= 0)
+	{
+		if ((n >> s) & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
 }
