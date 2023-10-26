ce96c0c11cd73ce802de29aa0303cbda6a2e3d21
ehisgodswill <ehisgodswill@gmail.com>
2023-10-26 16:46:12 +0100

    push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,32 +3,31 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int a = 0;
-    int f = 0;
-    int len = 0;
+	unsigned int a = 0;
+	int f = 0;
+	int len = 0;
 
-    while (b[f++])
-    {
-        len++;
-    }
-    f=0;
+	while (b[f++])
+	{
+		len++;
+	}
+	f = 0;
 
-    while (--len >= 0)
-    {
-        if (b[len] == '1')
-        {
-            a += pow(2, f);
-        }
-        else if (b[len] == '0')
-        {
-            
-        }
-        else
-        {
-            return 0;
-        }
-            
-        f++;
-    }
-    return a;
+	while (--len >= 0)
+	{
+		if (b[len] == '1')
+		{
+			a += pow(2, f);
+		}
+		else if (b[len] == '0')
+		{
+		}
+		else
+		{
+			return (0);
+		}
+
+		f++;
+	}
+	return (a);
 }
