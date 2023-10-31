e7809a0cf946560e49df1d42029f694865f9aa18
ehisgodswill <ehisgodswill@gmail.com>
2023-10-31 08:14:45 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,15 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
+	else
+	{
+		return (-1)
+	}
+	
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1 || wrt == -1)
 		return (-1);
 	close(fd);
 	return (1);
