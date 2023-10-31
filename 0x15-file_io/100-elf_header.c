72a08e7f8c9a5efccc969b25874e74d3272b7415
ehisgodswill <ehisgodswill@gmail.com>
2023-11-01 00:57:19 +0100

    c z

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,8 +10,6 @@ void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_title(const char *str);
 void print_class_data(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
 void print_abi_osabi(unsigned char *e_ident);
 void print_type_entry(unsigned int e_type, unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
@@ -131,15 +129,11 @@ void print_class_data(unsigned char *e_ident)
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
-}
 /**
  * print_version - Prints the version of an ELF header.
- * @e_ident: A pointer to an array containing the ELF version.
  */
-void print_version(unsigned char *e_ident)
-{
-	printf(" Version: %d",
-		   e_ident[EI_VERSION]);
+	print_title("Version:");
+	printf("%d", e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
@@ -315,9 +309,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class_data(header->e_ident);
-	print_version(header->e_ident);
-	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
+	print_abi_osabi(header->e_ident);
 	print_type_entry(header->e_type, header->e_entry, header->e_ident);
 
 	free(header);
