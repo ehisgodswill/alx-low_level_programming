6bf3b846f7c3057c813d467ce94290bd5bae1ef8
ehisgodswill <ehisgodswill@gmail.com>
2023-11-01 00:53:38 +0100

    fsvsdv

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -12,10 +12,8 @@ void print_title(const char *str);
 void print_class_data(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void print_abi_osabi(unsigned char *e_ident);
+void print_type_entry(unsigned int e_type, unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
@@ -158,9 +156,9 @@ void print_version(unsigned char *e_ident)
  * print_osabi - Prints the OS/ABI of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
  */
-void print_osabi(unsigned char *e_ident)
+void print_abi_osabi(unsigned char *e_ident)
 {
-	printf(" OS/ABI: \t\t\t\t");
+	print_title(" OS/ABI:");
 
 	switch (e_ident[EI_OSABI])
 	{
@@ -197,29 +195,26 @@ void print_osabi(unsigned char *e_ident)
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
-}
 
-/**
- * print_abi - Prints the ABI version of an ELF header.
- * @e_ident: A pointer to an array containing the ELF ABI version.
- */
-void print_abi(unsigned char *e_ident)
-{
-	printf(" ABI Version: \t\t\t\t%d\n",
-		   e_ident[EI_ABIVERSION]);
+	/**
+	 * print_abi - Prints the ABI version of an ELF header.
+	 */
+	print_title("ABI Version:");
+	printf("%d\n", e_ident[EI_ABIVERSION]);
 }
 
 /**
  * print_type - Prints the type of an ELF header.
  * @e_type: The ELF type.
+ * @e_entry: The address of the ELF entry point.
  * @e_ident: A pointer to an array containing the ELF class.
  */
-void print_type(unsigned int e_type, unsigned char *e_ident)
+void print_type_entry(unsigned int e_type, unsigned long int e_entry, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
-	printf(" Type: ");
+	print_title("Type:");
 
 	switch (e_type)
 	{
@@ -241,16 +236,12 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 	default:
 		printf("<unknown: %x>\n", e_type);
 	}
-}
 
-/**
- * print_entry - Prints the entry point of an ELF header.
- * @e_entry: The address of the ELF entry point.
- * @e_ident: A pointer to an array containing the ELF class.
- */
-void print_entry(unsigned long int e_entry, unsigned char *e_ident)
-{
-	printf(" Entry point address: ");
+	/**
+	 * print_entry - Prints the entry point of an ELF header.
+	 * @e_ident: A pointer to an array containing the ELF class.
+	 */
+	print_title("Entry point address:");
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
@@ -327,8 +318,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
 	print_abi(header->e_ident);
-	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
+	print_type_entry(header->e_type, header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(o);
